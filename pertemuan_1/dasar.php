ce32194acaaa43b7a28998ed38b2e7748b46a9b1
andisa19 <62782539+andisa19@users.noreply.github.com>
2021-03-10 20:20:26 +0700

    pertemuan_1

---
+++ pertemuan_1/dasar.php
@@ -0,0 +1,45 @@
+<?php
+   // echo"Hello World":
+   $angka = 1;
+   $contoh_str = "fsdfsd"; // string
+   $contoh_int = 1; // double
+    
+
+   // lokasi folder 
+   // C:xampp/
+   // localhost/project_kalian/pertemuan_1
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dasar PHP</title>
+</head>
+<body>
+    <?php for ($i=0; $i<5;$i++);?>
+	    <?php if($i % 2===0):?>
+		    <h2>Hello World</h2>
+	    <?php else : ?>
+		    <h1>
+    <?php
+        echo"Hello World":
+
+       
+        
+
+        $login = true 
+        $student =[
+            [
+                'nama'=> 'Dewangga'
+                'nim' =>
+                'usia'=>''
+            ],
+        ]:
+        
+        // if(student)
+            
+    ?>
+</body>
+</html> 
